0794902a9972d0e3d2d25f8ad60d88982ccd15b4
SOLOMON-PC\Solomon <solomon.iroegbu@gmail.com>
2021-06-13 21:39:28 +0100

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class AuthController extends Controller
+{
+    //Register method
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+
+    // Login Method
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+
+    // app/Http/Controllers/AuthController.php
+    //Fetch USer Details
+    public function show(Request $request)
+    {
+        return $request->user();
+        
+    }
+
+
+    //Logout
+    public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+
+        if ($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+
+        return response()->json([ 'data' => 'User logged out.' ], 200);
+    }
+
+    //Update User
+    public function update(Request $request, User $user)
+    {
+        $user ->update($request->all());
+
+        return response()->json($user);
+    }
+
+    //Delete User
+    public function delete(User $user)
+    {
+        $user->delete();
+
+        return response()->json(null, 204);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+ //   return $request->user();
+//});
+    
+    
+
+
+
+    //Route::post('/register', [AuthController::class, 'register']);
+
+    //Route::post('/login', [AuthController::class, 'login']);
+
+
+
+    //Route::post('/me', [AuthController::class, 'me']);
+
+    // accessible to only authenticated users
+    //Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+    Route::post('/register', 'AuthController@register');
+    Route::post('/login', 'AuthController@login');
+
+    Route::get('/show', 'AuthController@show')->middleware('auth:sanctum');
